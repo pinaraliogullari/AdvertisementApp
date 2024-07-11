650f13e8db543ca1fde54c7259fb0a5068bd8da2
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-07-11 15:04:44 +0300

    Unit of work design pattern implemented.

---
+++ AdvertisementApp/AdvertisementApp.Data/UnitOfWork/IUow.cs
@@ -0,0 +1,12 @@
+﻿using AdvertisementApp.Data.Interfaces;
+using AdvertisementApp.Entities;
+
+namespace AdvertisementApp.Data.UnitOfWork
+{
+    public interface IUow
+    {
+        IRepository<T> GetRepository<T>() where T:BaseEntity;
+        Task SaveChangesAsync();
+
+    }
+}
---
+++ AdvertisementApp/AdvertisementApp.Data/UnitOfWork/Uow.cs
@@ -0,0 +1,24 @@
+﻿using AdvertisementApp.Data.Contexts;
+using AdvertisementApp.Data.Interfaces;
+using AdvertisementApp.Data.Repositories;
+using AdvertisementApp.Entities;
+
+namespace AdvertisementApp.Data.UnitOfWork
+{
+    public class Uow
+    {
+        private readonly AdvertisementContext _context;
+
+        public Uow(AdvertisementContext context)
+        {
+            _context = context;
+        }
+
+        public IRepository<T> GetRepository<T>()where T:BaseEntity=>
+            new Repository<T>(_context);
+
+        public async Task SaveChangesAsync()=>
+            await _context.SaveChangesAsync();
+         
+    }
+}
