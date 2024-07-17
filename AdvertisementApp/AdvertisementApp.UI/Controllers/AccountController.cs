cb287704d354f4cf5fca6fee817e04a07f9a374a
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-07-17 10:37:13 +0300

    User registration form created.

---
+++ AdvertisementApp/AdvertisementApp.UI/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using AdvertisementApp.Business.Interfaces;
+using AdvertisementApp.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdvertisementApp.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IGenderManager _genderManager;
+
+        public AccountController(IGenderManager genderManager)
+        {
+            _genderManager = genderManager;
+        }
+
+        public  async Task<IActionResult> SignUp()
+        {
+            var response= await _genderManager.GetAllAsync();
+            //var genders = response.Data;
+            //List<SelectListItem> genderListItems = genders.Select(x => new SelectListItem
+            //{
+            //    Text = x.Definition,
+            //    Value = x.Id.ToString()
+            //}).ToList();
+            //UserCreateModel model = new UserCreateModel
+            //{
+            //    GenderListItems = genderListItems
+            //};
+            //veya
+            var model = new UserCreateModel
+            {
+                GenderList = new SelectList(response.Data, "Id", "Definition")
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserCreateModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ AdvertisementApp/AdvertisementApp.UI/Models/UserCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AdvertisementApp.UI.Models
+{
+    public class UserCreateModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string PhoneNumber { get; set; }
+        public int GenderId { get; set; }
+        public SelectList GenderList { get; set; }
+        //public List<SelectListItem> GenderListItems { get; set; }
+    }
+}
