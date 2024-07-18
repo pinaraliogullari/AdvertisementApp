1661b66208c89f6fc84fb250a959fca2b598948d
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-07-18 16:48:09 +0300

    Created application with text editor.

---
+++ AdvertisementApp/AdvertisementApp.UI/Controllers/ApplicationController.cs
@@ -0,0 +1,37 @@
+﻿using AdvertisementApp.Business.Interfaces;
+using AdvertisementApp.Dtos;
+using AdvertisementApp.UI.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertisementApp.UI.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class ApplicationController : Controller
+    {
+        private readonly IAdvertisementManager _advertisementManager;
+
+        public ApplicationController(IAdvertisementManager advertisementManager)
+        {
+            _advertisementManager = advertisementManager;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var response = await _advertisementManager.GetAllAsync();
+            return this.ResponseView(response);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AdvertisementCreateDto dto)
+        {
+            var response= await _advertisementManager.CreateAsync(dto);
+            return this.ResponseRedirectAction(response,"List");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # AdvertisementApp
  Advertisement application developed using Asp.Net Core.
+- N Tier Architecture
+- Unit of Work
+- Repository Design Pattern
+- Fluent Validation
+- Custom Cookie Based Authentication
+- Bootstrap
+- Entity Framework Core
+- Dependency Injection
+- AutoMapper
